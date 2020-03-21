80c644dd02301f57ac47ab857492ca2e357a4061
Artup1101 <60412299+Artup1101@users.noreply.github.com>
2020-03-21 12:15:25 +0700

    Add files via upload

---
+++ gg.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <style>
+        section{
+            min-height: 550px;
+        }
+    </style>   
+</head>
+<body>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-warning">   
+    <img src="https://www.bps.go.id/website/images/Header-Frontend-Kecil-ind.png;wa40b70b3f02234a67" alt="Slidebars Logo" >
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav  mr-auto">
+        <li class="nav-item active">
+            <div class="container">
+        <a class="nav-item nav-link" href="C:\xampp\htdocs\bootstrap\gg.php\home">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-item nav-link" href="C:\xampp\htdocs\bootstrap\gg.php\#dash">Dashboard</a>
+        <a class="nav-item nav-link" href="C:\xampp\htdocs\bootstrap\gg.php\#home">Contact Us</a>
+        </div>
+    </li>
+    </div>
+</div>
+</nav>
+<div class="home">
+    <section id="home" class="home">
+    <div class="jumbotron text-center " height="600">
+    <img src="inijugajugagambarbps.png" width="400" class="rounded-circle">
+  <h1 class="display-4">Badan Pusat Statistik</h1><br><br>
+  <p class="lead">Statistik Perdagangan Indonesia | Sensus Penduduk Indonesia</p>
+  <hr class="my-4">
+  <p>Lembaga yang mengatur dan Mencatat Statistik Perdagangan dan Jumlah Penduduk Indonesia</p>
+</div>
+    </section>
+</div>
+<div class="dash">    
+    <section id="dash" class="dash">
+    <div class="container mt-6">
+            <div class="row mt-10">
+                <div class="col-md text-center">
+                    <br><br>
+                    <h2><i class="fas fa-chart-line mr-2"></i>Dashboard</h2>
+                    <hr class="bg-light">
+                    <br><br><br><br>
+                </div>
+            </div>
+            <div class="row">
+            <div class="col-md-4">
+                <div class="card bg-danger" style="width: 18rem;">
+                    <div class="card-body text-white">
+                        <h5 class="card-title">Jumlah Anak-Anak </h5>
+                        <p class="card-text" style="font-size:60px">66,17 Juta Jiwa</p>
+                        <a href="http://localhost/siperpus/buku/index.php" class="text-white">Lebih detail <i class="fas fa-angle-double-right"></i></a>
+                    </div>
+                </div> 
+            </div>
+            <div class="col-md-4">
+                <div class="card bg-warning" style="width: 18rem;">
+                    <div class="card-body text-white">
+                        <h5 class="card-title">Jumlah Orang Dewasa</h5>
+                        <p class="card-text" style="font-size:60px">200,8 Juta Jiwa</p>
+                        <a href="http://localhost/siperpus/anggota/index.php" class="text-white">Lebih detail <i class="fas fa-angle-double-right"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card bg-primary" style="width: 18rem;">
+                    <div class="card-body text-white">
+                        <h5 class="card-title">Jumlah Warga Indonesia</h5>
+                        <p class="card-text" style="font-size:60px">267,00 Juta Jiwa</p>
+                        <a href="http://localhost/siperpus/transaksi/index.php" class="text-white">Lebih detail <i class="fas fa-angle-double-right"></i></a>
+                    </div>
+            </div>
+        </div>
+</div>
+
+<div class="row ">
+            <div class="col-md-4">
+            <div class="container">   
+            <br>
+            <img src="jumlahpenduduk.png" width="500">
+            <br>
+            </div>
+    </div>
+</div>
+    </section>
+    </div>
+    <div class="con">
+<section id="con" class="con">
+<div class="jumbotron text-center">
+  <h1 class="display-4">Contact US</h1><br><br><br><br>
+  <p class="lead">Facebook : <a href="https://www.facebook.com/profile.php?id=100011144283865">Msayp Putra<a> Instagram : <a href="https://www.instagram.com/msayp_putra3105/">@Msayp_Putra3105<a> Whatsapp : 081555356636 </p>
+  <hr class="my-4">
+  <p>Alamat : Jalan Danau Kerinci G6F No.22 Sawojajar Malang Jawa Timur Indonesia</p>
+  <br><br><br><br><br>
+  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  <br><br>
+</div>
+    </section>
+    </div>
+<footer class = "container bg-dark text-light"widt=700>
+    <center><p>Copyright@2020</p></center>
+    </footer>        
+</body>
+
+</html>
